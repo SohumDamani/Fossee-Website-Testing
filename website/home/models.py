995d3603dd02c7eee63eb33692a269567a1c13bf
SohumDamani <damanisohum@gmail.com>
2022-05-25 22:43:45 +0530

    Added Navbar,Sidebar,Footer snippets and a test page

--- website/home/models.py
+++ website/home/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django import forms
+
+from modelcluster.fields import ParentalManyToManyField
 
 from streams import blocks
 
 from wagtail.models import Page
 from wagtail.fields import StreamField
-from wagtail.admin.edit_handlers import FieldPanel,MultiFieldPanel,FieldRowPanel
+from wagtail.admin.edit_handlers import FieldPanel,MultiFieldPanel,InlinePanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
 class HomePage(Page):
 
@@ -78,3 +85,72 @@ class FlexPage(Page):
         FieldPanel('content')
     ]
 
+@register_snippet
+class Navbar(models.Model):
+    title = models.CharField(unique=True,max_length=255)
+    logo = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.CASCADE, related_name='+'
+    )
+    links = StreamField([
+        ('navs',blocks.LinkBlock())
+    ],null=True,blank=True)
+
+    panels = [FieldPanel('title'),
+              ImageChooserPanel('logo'),
+              FieldPanel('links',heading='Nav Links',classname='collapsible')]
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Navbar"
+        verbose_name_plural = "Navbars"
+
+@register_snippet
+class Sidebar(models.Model):
+    title = models.CharField(unique=True,max_length=255)
+    sidebar = StreamField(
+        [
+            ('sidebar',blocks.SidebarBlock())
+        ],null=True,blank=True)
+    panels = [FieldPanel('title'),
+              FieldPanel('sidebar')]
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Sidebar"
+        verbose_name_plural = "Sidebars"
+
+@register_snippet
+class Footer(models.Model):
+    title = models.CharField(unique=True,max_length=255)
+    footer = StreamField(
+        [
+            ('contents', blocks.FooterBlock()),
+            ('extlinks',blocks.SidebarBlock(label='External Links',icon='link'))
+        ],null=True,blank=True)
+
+    panels = [FieldPanel('title'),
+              FieldPanel('footer')]
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Footer"
+        verbose_name_plural = "Footers"
+
+
+class TestPage(Page):
+    navbar = ParentalManyToManyField(Navbar,null=True,blank=True)
+    sidebar = ParentalManyToManyField(Sidebar,null=True,blank=True)
+    footer = ParentalManyToManyField(Footer,null=True,blank=True)
+
+    content_panels = Page.content_panels+[
+        FieldPanel('navbar',widget=forms.CheckboxSelectMultiple),
+        FieldPanel('sidebar',widget=forms.CheckboxSelectMultiple),
+        FieldPanel('footer',widget=forms.CheckboxSelectMultiple),
+    ]
